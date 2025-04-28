b6787a2d26882b6401589bc6f29207211afc7e51
Simranjit Singh <simranjitsingh58648@gmail.com>
2025-04-28 07:21:19 +0000

    initial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("prjmania.projectmania.controller.View1", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["prjmania/projectmania/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
