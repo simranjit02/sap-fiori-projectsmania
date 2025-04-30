5794a44b784b8b07a183bacc7ac2a19ad3268751
simranjit02 <simranjitsingh58648@gmail.com>
2025-04-30 18:55:44 +0300

    work on initial setup

---
+++ webapp/controller/Tiles.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/core/mvc/controller"], (controller) => {
+  return controller.extend("prjmania.projectmania.controller.Tiles", {
+    onInit() {
+      console.log("123", 123);
+    },
+  });
+});
