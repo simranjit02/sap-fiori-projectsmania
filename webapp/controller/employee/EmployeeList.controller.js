c67c5f704dd228674c8bb60febf6478b4e63fcb6
simranjit02 <simranjitsingh58648@gmail.com>
2025-05-05 20:29:41 +0300

    integrate model

--- webapp/controller/employee/EmployeeList.controller.js
+++ webapp/controller/employee/EmployeeList.controller.js
@@ -1,8 +1,23 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  return Controller.extend(
-    "prjmania.projectmania.controller.employee.EmployeeList",
-    {
-      onInt() {},
-    }
-  );
-});
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "prjmania/projectmania/model/models",
+  ],
+  (Controller, JSONModel, Models) => {
+    return Controller.extend(
+      "prjmania.projectmania.controller.employee.EmployeeList",
+      {
+        onInit() {
+          //   var oEmployeeModel = new JSONModel("../../model/employees.json");
+
+          //   console.log("oEmployeeModel", oEmployeeModel);
+          //   sap.ui.getCore().setModel(oEmployeeModel, "employee");
+          var oEmployeeModel = Models.createJSONModel();
+          console.log("oEmployeeModel", oEmployeeModel);
+          sap.ui.getCore().setModel(oEmployeeModel, "employee");
+        },
+      }
+    );
+  }
+);
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,20 +1,23 @@
-sap.ui.define([
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/Device"
-], 
-function (JSONModel, Device) {
+sap.ui.define(
+  ["sap/ui/model/json/JSONModel", "sap/ui/Device"],
+  function (JSONModel, Device) {
     "use strict";
 
     return {
-        /**
-         * Provides runtime information for the device the UI5 app is running on as a JSONModel.
-         * @returns {sap.ui.model.json.JSONModel} The device model.
-         */
-        createDeviceModel: function () {
-            var oModel = new JSONModel(Device);
-            oModel.setDefaultBindingMode("OneWay");
-            return oModel;
-        }
+      /**
+       * Provides runtime information for the device the UI5 app is running on as a JSONModel.
+       * @returns {sap.ui.model.json.JSONModel} The device model.
+       */
+      createDeviceModel: function () {
+        var oModel = new JSONModel(Device);
+        oModel.setDefaultBindingMode("OneWay");
+        return oModel;
+      },
+      createJSONModel: function () {
+        var oEmployeeModel = new JSONModel();
+        oEmployeeModel.loadData("model/employees.json");
+        return oEmployeeModel;
+      },
     };
-
-});
+  }
+);
